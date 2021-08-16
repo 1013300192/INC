576b6147bb70ca73aa661bef6f78ce1fbb1873be
Destiny <1013300192@qq.com>
2021-08-16 21:45:28 +0800

    Test 1

---
+++ app/src/main/java/com/example/intelligentnavigationcane/GHJ/Test_0.java
@@ -0,0 +1,16 @@
+package com.example.intelligentnavigationcane.GHJ;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.intelligentnavigationcane.R;
+
+public class Test_0 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test0);
+    }
+}
