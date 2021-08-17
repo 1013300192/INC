1bc2c2241a69278c11c47fb34f1b7d6ca51e4d90
Destiny <1013300192@qq.com>
2021-08-17 22:59:52 +0800

    Y 8_17

---
+++ app/src/main/java/com/example/intelligentnavigationcane/YBF/Personal_Information.java
@@ -0,0 +1,16 @@
+package com.example.intelligentnavigationcane.YBF;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.intelligentnavigationcane.R;
+
+public class Personal_Information extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personal_information);
+    }
+}
