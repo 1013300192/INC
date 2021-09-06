129d84045d036d7ece4b1b430a51e29fa36d982f
Destiny <1013300192@qq.com>
2021-09-07 02:45:15 +0800

    G.9_07

--- app/src/main/java/com/example/intelligentnavigationcane/GHJ/Test_Login/RegisterActivity.java
+++ app/src/main/java/com/example/intelligentnavigationcane/GHJ/Test_Login/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,7 +22,9 @@ import java.sql.SQLException;
 
 
 public class RegisterActivity extends AppCompatActivity {
-    public static int conn_on = 0;//用于判断连接是否成功
+    public static int conn_on= 0;//用于判断连接是否成功
+    public static int re_call=2;
+    public static int call=0;
     public static String password_receive;//用于接收数据库查询的返回数据
 
     @Override
@@ -32,36 +35,31 @@ public class RegisterActivity extends AppCompatActivity {
         final EditText username =  findViewById(R.id.edit_Rname);//取得输入框的对象
         final EditText password =  findViewById(R.id.edit_Rpass);
 
-        final TextView connR =  findViewById(R.id.conn_R);//取得网络提示框的对象、
-        connR.setBackgroundColor(Color.RED);//默认设成红色
+
         final Handler handler = new Handler(new Handler.Callback() {
             @Override
-            public boolean handleMessage(Message message) {
-                switch (conn_on)//根据返回值判断网络连接是否成功
-                {
-                    case 1:
-                        connR.setText("网络连接成功");
-                        connR.setBackgroundColor(Color.GREEN);
-                        break;
-                    case 2:
-                        connR.setText("网络连接失败");
-                        break;
+            public boolean handleMessage(Message msg) {
+                if(conn_on==1){
+                    if(re_call==1){
+                        if(call==1){
+                            Toast.makeText(RegisterActivity.this,"注册成功",Toast.LENGTH_SHORT).show();
+                            Intent intent=new Intent(RegisterActivity.this,Main_LoginActivity.class);
+                            startActivity(intent);}
+                        else if(call==0){
+                            Toast.makeText(RegisterActivity.this,"信息未填写完整",Toast.LENGTH_SHORT).show();
+                        }
+                    }else if(re_call==0) {
+                        Toast.makeText(RegisterActivity.this,"用户名已存在",Toast.LENGTH_SHORT).show();
+                    }
+                }else if(conn_on==0){
+                    Toast.makeText(RegisterActivity.this,"网络未连接",Toast.LENGTH_SHORT).show();
                 }
-                return false;
+
+
+
+                return true;
             }
         });
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                Message msg = new Message();
-                try {
-                    connect.getConnection("inc");//执行连接测试
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-                handler.sendMessage(msg);//跳转到handler1
-            }
-        }).start();
         Button Register = findViewById(R.id.btn_ready);
         Register.setOnClickListener(new View.OnClickListener() {//注册
             @Override
@@ -69,18 +67,19 @@ public class RegisterActivity extends AppCompatActivity {
                 new Thread(new Runnable() {
                     @Override
                     public void run() {
+                        Message msg = new Message();
                         try {
-                            connect.insertIntoData(username.getText().toString(),password.getText().toString());//调用插入数据库语句
-                        } catch (SQLException e) {
+                            connect.insertIntoData(username.getText().toString(), password.getText().toString());//判断用户名是否存在再插入数据
+                        }catch (SQLException e){
                             e.printStackTrace();
                         }
-                    }
+                        handler.sendMessage(msg);
+                        }
                 }).start();
-                Toast.makeText(RegisterActivity.this,"注册成功",Toast.LENGTH_SHORT).show();
-                Intent intent=new Intent(RegisterActivity.this,Main_LoginActivity.class);
-                startActivity(intent);
+
             }
         });
     }
-}
+    }
+
 
--- app/src/main/java/com/example/intelligentnavigationcane/connect.java
+++ app/src/main/java/com/example/intelligentnavigationcane/connect.java
@@ -1,5 +1,8 @@
 package com.example.intelligentnavigationcane;
 
+import android.text.TextUtils;
+import android.widget.Toast;
+
 import com.example.intelligentnavigationcane.GHJ.Test_Login.Main_LoginActivity;
 import com.example.intelligentnavigationcane.GHJ.Test_Login.RegisterActivity;
 
@@ -11,49 +14,90 @@ import java.sql.Statement;
 
 public class connect {
     public static Connection getConnection(String dbName) throws SQLException {
-        Connection conn = null;
-        try {
-            Class.forName("com.mysql.jdbc.Driver"); //加载驱动
-            String ip = "rm-bp1bk64541tej95g6oo.mysql.rds.aliyuncs.com";
-            conn =(Connection) DriverManager.getConnection(
-                    "jdbc:mysql://rm-bp1bk64541tej95g6oo.mysql.rds.aliyuncs.com:3306/inc",
-                    "ybf_1013300192", "yin@091123");
-            Main_LoginActivity.conn_on=1;//用于向主函数传参，判断连接是否成功
-            RegisterActivity.conn_on=1;
-        }catch (SQLException | ClassNotFoundException ex) {
-            ex.printStackTrace();
-            Main_LoginActivity.conn_on=2;//用于向主函数传参，判断连接是否成功
-            RegisterActivity.conn_on=2;
+            Connection conn ;
+            try {
+                Class.forName("com.mysql.jdbc.Driver"); //加载驱动
+                String ip = "rm-bp1bk64541tej95g6oo.mysql.rds.aliyuncs.com";
+                conn =(Connection) DriverManager.getConnection(
+                        "jdbc:mysql://rm-bp1bk64541tej95g6oo.mysql.rds.aliyuncs.com:3306/inc",
+                        "ybf_1013300192", "yin@091123");
+
+            }catch (SQLException | ClassNotFoundException ex) {
+                conn=null;
+                ex.printStackTrace();
+            }
+            return conn;//返回Connection型变量conn用于后续连接
         }
-        return conn;//返回Connection型变量conn用于后续连接
-    }
     public static int insertIntoData(final String username, final String password) throws SQLException {//增加数据
-        Connection  conn = null;
+        Connection conn ;
         conn = getConnection("inc");
-        //使用DriverManager获取数据库连接
-        Statement stmt = conn.createStatement();
-        //使用Connection来创建一个Statment对象
-        String sql = "insert INTO person(username,password)VALUES('"+username+"','"+password+"')";//把用户名和密码插入到数据库中
-        return stmt.executeUpdate(sql);
-        //执行DML语句，返回受影响的记录条数
+        if(conn!=null) {
+            RegisterActivity.conn_on=1;
+            //使用DriverManager获取数据库连接
+            Statement stmt = conn.createStatement();
+            //使用Connection来创建一个Statment对象
+            ResultSet rs;//从数据库中查询用户名对应的密码并返回
+            rs = stmt.executeQuery(
+                    "select * from person where username ='" + username + "'");
+            if (rs.next()) {
+                RegisterActivity.re_call = 0;
+            } else {
+                RegisterActivity.re_call = 1;
+                if(!TextUtils.isEmpty(username)&&!TextUtils.isEmpty(password)){
+                    RegisterActivity.call=1;
+                    String sql = "insert INTO person(username,password)VALUES('" + username + "','" + password + "')";//把用户名和密码插入到数据库中
+                    return stmt.executeUpdate(sql);
+                }else {
+                    RegisterActivity.call=0;
+                }
+            }
+        }else {
+            RegisterActivity.conn_on=0;
+        }
+        return 0;
     }
-    public static String querycol(final String id) throws SQLException {//读取某一行
+        //执行DML语句，返回受影响的记录条数
+
+   /*public static String querycol_P(final String id) throws SQLException {//读取某一行
         //加载数据库驱动
         String a;
-        Connection conn = null;
+        Connection conn ;
         conn = getConnection("inc");
         //使用DriverManager获取数据库连接
-        Statement stmt = conn.createStatement();
-        //使用Connection来创建一个Statment对象
-        ResultSet rs;//从数据库中查询用户名对应的密码并返回
-        rs = stmt.executeQuery(
-                "select password from person where username='"+id+"'");
-        rs.first();
-        a=rs.getString(1);
-        rs.close();
-        return a;
-        //把查询结果输出来
+       Statement stmt = conn.createStatement();
+       //使用Connection来创建一个Statment对象
+       ResultSet rs1;//从数据库中查询用户名对应的密码并返回
+       rs1 = stmt.executeQuery(
+               "select password from person where username='"+id+"'");
+       rs1.first();
+       a=rs1.getString(1);
+       rs1.close();
+       return a;
+       //把查询结果输出来
+    }*/
+
+    public static String Recall_Data(final String username,final String password) throws SQLException {//增加数据
+        Connection  conn ;
+        conn = getConnection("inc");
+        if(conn!=null){
+            Main_LoginActivity.conn_on=1;
+            //使用DriverManager获取数据库连接
+            Statement stmt = conn.createStatement();
+            //使用Connection来创建一个Statment对象
+            ResultSet rs;//从数据库中查询用户名对应的密码并返回
+            rs = stmt.executeQuery(
+                    "select username and password from person where username ='" + username + "'and password='"+password+"'");
+            if(rs.next()){
+                Main_LoginActivity.Recall=1;//表示登录成功
+            }else{
+                Main_LoginActivity.Recall=0;//表示密码错误
+            }
+        }else {
+            Main_LoginActivity.conn_on=0;
+        }
+        return null;
     }
+
 }
 
 
